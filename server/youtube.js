6881f416f0d258139513907906bbbe1e7dae8846
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2016-09-24 21:30:09 -0400

    include new youtube

---
+++ server/youtube.js
@@ -0,0 +1,57 @@
+'use strict'
+
+const search = require('youtube-search')
+const log = require('debug')('r-fi:search')
+const youtubeStream = require('youtube-audio-stream')
+
+const API_KEY = process.env.API_KEY
+if (!API_KEY) {
+  log('Missing youtube API_KEY - exiting...')
+  process.exit()
+}
+
+const opts = {
+  maxResults: 5,
+  key: API_KEY
+}
+
+module.exports.search = function (value, fn) {
+  search(value, opts, (err, results) => {
+    if (err) {
+      return fn(err)
+    }
+
+    return fn(null, results.map(r => processResult(r)))
+  })
+}
+
+function processResult (res) {
+  return {
+    link: res.link, // https://youtube.com/watch?v=${id}
+    title: res.title, // string
+    description: res.description, // string
+    thumbnail: res.thumbnails.default.url
+  }
+}
+
+const Youtube = require('youtube-node');
+const youtube = new Youtube();
+
+youtube.setKey(API_KEY)
+
+module.exports.infoFor = function (link, fn) {
+  const id = link.split('=')[1]
+
+  youtube.getById(id, (err, result) => {
+    if (err) {
+      log(JSON.stringify(err))
+      return fn(err)
+    }
+
+    return fn(null, result)
+  })
+}
+
+module.exports.streamAudio = function (link) {
+  return youtubeStream(link)
+}
